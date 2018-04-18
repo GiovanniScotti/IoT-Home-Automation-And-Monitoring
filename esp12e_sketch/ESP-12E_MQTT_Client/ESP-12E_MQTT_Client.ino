f8753836315bc09d8a3b218e11d7159d597fd662
Giovanni Scotti <carlo.scotti.99@alice.it>
2018-04-18 17:34:43 +0200

    Adds first draft of ESP-12E sketch

---
+++ esp12e_sketch/ESP-12E_MQTT_Client/ESP-12E_MQTT_Client.ino
@@ -0,0 +1,97 @@
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+#define WLAN_SSID   "WLANNAME"
+#define WLAN_PASS   "PASSWORD"
+
+//MQTT broker settings
+#define HOST        "192.168.0.10"
+#define PORT        1883
+#define USERNAME    "YOUR BROKER USERNAME"
+#define PASSWORD    "YOUR BROKER PASSWORD"
+
+//milliseconds to sleep
+const int intervall = 5000;
+//time out loop count
+const int timeout = 200;
+
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, HOST, PORT, USERNAME, PASSWORD);
+Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt, "weatherStation/temperature");
+/*
+Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt, "weatherStation/temperature");
+Adafruit_MQTT_Publish pressure = Adafruit_MQTT_Publish(&mqtt, "weatherStation/pressure");
+Adafruit_MQTT_Publish humidity = Adafruit_MQTT_Publish(&mqtt, "weatherStation/humidity");
+Adafruit_MQTT_Publish light = Adafruit_MQTT_Publish(&mqtt, "weatherStation/light");
+Adafruit_MQTT_Publish voltage = Adafruit_MQTT_Publish(&mqtt, "weatherStation/voltage");
+*/
+
+
+// IF DEBUG SET then SERIAL else REMOVE SERIAL
+
+void setup() {
+  WiFi.forceSleepWake();
+  delay(1);
+  WiFi.mode(WIFI_STA);  
+
+  // ***** DEBUG
+  Serial.begin(115200);
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  int i = 0;
+  for (; i < timeout; i++)
+  {
+    if(WiFi.status() == WL_CONNECTED) break;
+    delay(100);
+    //Serial.print(".");
+  }
+  if(i == timeout)
+    deepSleep();
+
+// ***** DEBUG
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+
+  MQTT_connect();
+  delay(100);
+  temperature.publish("20.0");
+
+  // ***** DEBUG
+  Serial.println("deep sleep");
+  deepSleep();
+}
+
+void deepSleep()
+{
+  //https://github.com/esp8266/Arduino/issues/644
+  WiFi.mode(WIFI_OFF);
+  WiFi.forceSleepBegin();
+  delay(1);
+  ESP.deepSleep(intervall * 1000/*, WAKE_RF_DISABLED*/);
+}
+
+void loop() 
+{
+}
+
+void MQTT_connect() {
+  int8_t ret;
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+  uint8_t retries = 10;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 1 second...");
+       mqtt.disconnect();
+       delay(1000);
+       retries--;
+       if (retries == 0)
+         deepSleep();
+  }
+  Serial.println("MQTT Connected!");
+}
