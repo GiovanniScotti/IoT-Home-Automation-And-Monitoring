68cf653abfe43f1f19f4d1ca6efa6c724dd9d2d9
Giovanni Scotti <carlo.scotti.99@alice.it>
2018-04-18 20:37:47 +0200

    Adds MQTT support

--- esp12e_sketch/ESP-12E_MQTT_Client/ESP-12E_MQTT_Client.ino
+++ esp12e_sketch/ESP-12E_MQTT_Client/ESP-12E_MQTT_Client.ino
@@ -1,66 +1,84 @@
+// ***** IMPORT LIBRARIES *****
 #include <ESP8266WiFi.h>
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 
-#define WLAN_SSID   "WLANNAME"
-#define WLAN_PASS   "PASSWORD"
+// ***** Enable debug over serial communication
+#define DEBUG
 
-//MQTT broker settings
-#define HOST        "192.168.0.10"
+// Access point settings
+#define WLAN_SSID   ""
+#define WLAN_PASS   ""
+
+// MQTT broker settings
+#define HOST        "192.168.1.50"
 #define PORT        1883
-#define USERNAME    "YOUR BROKER USERNAME"
-#define PASSWORD    "YOUR BROKER PASSWORD"
+#define USERNAME    "admin"
+#define PASSWORD    "hamrpi"
 
-//milliseconds to sleep
-const int intervall = 5000;
-//time out loop count
-const int timeout = 200;
+// Seconds to sleep
+#define SLEEP_TIME  5
+
+// Number of attempts to get connection before entering sleep mode
+#define CONN_ATTEMPTS 200
+#define MQTT_ATTEMPTS 10
 
 WiFiClient client;
 Adafruit_MQTT_Client mqtt(&client, HOST, PORT, USERNAME, PASSWORD);
-Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt, "weatherStation/temperature");
-/*
-Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt, "weatherStation/temperature");
-Adafruit_MQTT_Publish pressure = Adafruit_MQTT_Publish(&mqtt, "weatherStation/pressure");
-Adafruit_MQTT_Publish humidity = Adafruit_MQTT_Publish(&mqtt, "weatherStation/humidity");
-Adafruit_MQTT_Publish light = Adafruit_MQTT_Publish(&mqtt, "weatherStation/light");
-Adafruit_MQTT_Publish voltage = Adafruit_MQTT_Publish(&mqtt, "weatherStation/voltage");
-*/
+Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt, "espnode1/temperature");
 
 
-// IF DEBUG SET then SERIAL else REMOVE SERIAL
+// TODO: add new publications
+
+void MQTT_connect();
 
 void setup() {
-  WiFi.forceSleepWake();
-  delay(1);
-  WiFi.mode(WIFI_STA);  
+  // Enable on-board ESP8266 wifi module to connect
+  // to the Access Point specified by WLAN settings
+  WiFi.mode(WIFI_STA);
 
-  // ***** DEBUG
-  Serial.begin(115200);
-  Serial.print("Connecting to ");
-  Serial.println(WLAN_SSID);
+  #ifdef DEBUG
+    Serial.begin(115200);
+    Serial.print("Connecting to ");
+    Serial.println(WLAN_SSID);
+  #endif
 
   WiFi.begin(WLAN_SSID, WLAN_PASS);
-  int i = 0;
-  for (; i < timeout; i++)
-  {
+
+  int i;
+  for (i = 0; i < CONN_ATTEMPTS; i++) {
     if(WiFi.status() == WL_CONNECTED) break;
     delay(100);
-    //Serial.print(".");
-  }
-  if(i == timeout)
-    deepSleep();
 
-// ***** DEBUG
-  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+    #ifdef DEBUG
+      Serial.print(".");
+    #endif
+  }
+  
+  if(i == CONN_ATTEMPTS) {
+    #ifdef DEBUG
+      Serial.println("TIMOUT!");
+    #endif
+    
+    // TODO: and enter sleep mode
+    
+  }
+  
+  #ifdef DEBUG
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  #endif
 
   MQTT_connect();
   delay(100);
+  
   temperature.publish("20.0");
 
-  // ***** DEBUG
-  Serial.println("deep sleep");
-  deepSleep();
+  #ifdef DEBUG
+    Serial.println("deep sleep");
+  #endif
+  
+  delay(3000);
 }
 
 void deepSleep()
@@ -69,29 +87,45 @@ void deepSleep()
   WiFi.mode(WIFI_OFF);
   WiFi.forceSleepBegin();
   delay(1);
-  ESP.deepSleep(intervall * 1000/*, WAKE_RF_DISABLED*/);
+  ESP.deepSleep(SLEEP_TIME * 1000000/*, WAKE_RF_DISABLED*/);
 }
 
 void loop() 
 {
+  temperature.publish("20.0");
+
+  // ***** DEBUG
+  Serial.println("deep sleep");
+  delay(3000);
+  
 }
 
 void MQTT_connect() {
-  int8_t ret;
-  if (mqtt.connected()) {
+  int8_t err_code;
+  
+  if(mqtt.connected()) {
     return;
   }
 
-  Serial.print("Connecting to MQTT... ");
-  uint8_t retries = 10;
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-       Serial.println(mqtt.connectErrorString(ret));
-       Serial.println("Retrying MQTT connection in 1 second...");
-       mqtt.disconnect();
-       delay(1000);
-       retries--;
-       if (retries == 0)
-         deepSleep();
+  #ifdef DEBUG
+    Serial.print("Connecting to MQTT... ");
+  #endif
+  
+  uint8_t retries = MQTT_ATTEMPTS;
+  while ((err_code = mqtt.connect()) != 0) { // Connect returns 0 on success
+    #ifdef DEBUG
+      Serial.println(mqtt.connectErrorString(err_code));
+      Serial.println("Retrying MQTT connection in 1 second...");
+    #endif
+    
+    mqtt.disconnect();
+    delay(1000);
+    retries--;
+    if(retries == 0)
+      deepSleep();
   }
-  Serial.println("MQTT Connected!");
+
+  #ifdef DEBUG
+    Serial.println("MQTT Connected!");
+  #endif
 }
